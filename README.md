757a475293ab93f781a637016df2f1848d9b2fbf
Elio Struyf <info@estruyf.be>
2019-02-27 16:09:06 +0100

    Initial commit of the spfx-navigation dependency

---
+++ README.md
@@ -0,0 +1,31 @@
+# Managing navigation in SharePoint Framework projects
+
+[![NPM](https://nodei.co/npm/spfx-navigation.png?compact=true)](https://nodei.co/npm/spfx-navigation/)
+
+SharePoint Framework handles it own navigation events. If you want to redirect from your code to another page, it will trigger a full page reload when you use `location.href = "<your-url>"`. This dependency allows you to define how you want to navigate to the page with partial or full page reloads.
+
+## Installation
+
+Run the following command to install the `spfx-navigation` dependency:
+
+```
+npm i spfx-navigation -S -E
+```
+
+## Usage
+
+Once the dependency is installed to your project, you can make use of it as follows:
+
+```typescript
+import { Navigation } from 'spfx-navigation';
+
+/* Navigation with partial page reload */
+Navigation.navigate("<your-url>");
+
+/* Navigation with full page reload */
+Navigation.navigate("<your-url>", true);
+```
+
+## Issues
+
+Experiencing issues? Please add these to the dependency its issue list: [SPFx navigation issues](https://github.com/estruyf/spfx-navigation/issues).
---
+++ src/index.ts
@@ -0,0 +1,58 @@
+
+export class Navigation {
+
+  /**
+   * Navigate to the provided link
+   * 
+   * @param {string} link - Link to navigate to
+   * @param {boolean} fullPageReload - (optional) partial page reload - false / or full page reload - true
+   */
+  public static navigate(link: string, fullPageReload: boolean = false) {
+    // Check if we can bind into the SPFx navigation APIs
+    if (!fullPageReload && history && (window["PopStateEvent"] || window["Event"]) && window["dispatchEvent"]) {
+      // Create the new navigation state
+      const navState = { url: link };
+
+      // Adds the new navigation state to the browser history
+      history.pushState(navState, null, link);
+
+      // Check to trigger SharePoint navigation handler to partially reload the page
+      let newPopState = null;
+      const popStateString = "popstate";
+      const eventString = "Event";
+      const stateString = "state";
+      try {
+        if (window["PopStateEvent"]) {
+          newPopState = new PopStateEvent(popStateString, { state: navState });
+        }
+
+        if (window[eventString] && !newPopState) {
+          newPopState = new Event(popStateString);
+          newPopState[stateString] = navState;
+        }
+
+        if (!newPopState) {
+          newPopState = new PopStateEvent(popStateString, {
+            bubbles: false,
+            cancelable: true,
+            state: navState
+          });
+        }
+      } catch (e) {
+        newPopState = document.createEvent(eventString);
+        newPopState.initEvent(popStateString, false, true);
+        newPopState[stateString] = navState;
+      }
+
+      if (newPopState) {
+        const isDispatched = window.dispatchEvent(newPopState);
+        if (isDispatched) {
+          return;
+        }
+      }
+    }
+
+    // Worst case, redirect the old way
+    location.href = link;
+  }
+}
